dd6c86286b1fa767c875af484e52e5f856fdc51e
Pol Renalias <95927097+polrenalias@users.noreply.github.com>
2022-10-03 17:32:32 +0200

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,85 +1,113 @@
 import random, pyaztro
-from telegram.ext import (Updater, CommandHandler)
+from telegram.ext import Updater, CommandHandler
 from telegram import InputMediaPhoto
 
+# Starting condition checker
 started = False
 
+# Bot starter function
 def start(update, context):
     global started
     username = str(update.message.from_user.first_name)
-    context.bot.send_message(update.message.chat_id, 'Welcome, '+username+", I'm FortuneBot.")
-    context.bot.send_message(update.message.chat_id, 'Select the theme (love, friendship, money, work, family) you want to consult about with /select theme')
+    context.bot.send_message(update.message.chat_id, 'Welcome, '+username+", I'm FortuneBot.\nYou can use me to consult about your astrological /sign, your /tarot fortune or today's /horoscope.")
     started = True
 
+# Function used to give the user its fortune via tarot card reading
 def get_tarot(update, context):
     global started
     if started == True:	
-        user_selection = ' '.join(context.args)
-        themes = ['love','money','friendship','work','family']
-        media_group = []
-        if user_selection in themes:
-            card_selection = random.sample(range(0,21), 3)
-            for num in card_selection:
-                if random.randint(0, 1) == 0:
-                    media_group.append(InputMediaPhoto(open('good_cards/%d.jpg' % num, 'rb'), caption = 'cards' if num == 0 else ''))
-                else:
-                    media_group.append(InputMediaPhoto(open('bad_cards/%d.jpg' % num, 'rb'), caption = 'cards' if num == 0 else ''))
-            context.bot.send_media_group(update.message.chat_id, media = media_group)  
+        if len(context.args) == 1:
+            theme = ' '.join(context.args).lower()
+            media_group = []
+            if theme in ['love','money','friendship','work','family']:
+                card_selection = random.sample(range(0,21), 3)
+                for num in card_selection:
+                    if random.randint(0, 1) == 0:
+                        media_group.append(InputMediaPhoto(open('good_cards/%d.jpg' % num, 'rb'), caption = 'cards' if num == 0 else ''))
+                    else:
+                        media_group.append(InputMediaPhoto(open('bad_cards/%d.jpg' % num, 'rb'), caption = 'cards' if num == 0 else ''))
+                context.bot.send_media_group(update.message.chat_id, media = media_group)
+            else:
+                context.bot.send_message(update.message.chat_id, 'Your selection needs to match one of the following themes: love, money, work, family')
+        elif len(context.args) == 0:
+            context.bot.send_message(update.message.chat_id, 'Usage: /tarot theme')
+        else:
+            context.bot.send_message(update.message.chat_id, 'Too many arguments. Try again or consult /help')
 
+# Function used to give the current day horoscope to the user (based on his astrological sign)
 def get_horoscope(update, context):
-    sign = ' '.join(context.args)
-    horoscope = pyaztro.Aztro(sign)
-    
-    context.bot.send_message(update.message.chat_id, 'Description: '+horoscope.description)
-    context.bot.send_message(update.message.chat_id, 'Mood: '+horoscope.mood)
-    context.bot.send_message(update.message.chat_id, 'Lucky color: '+horoscope.color)
-    context.bot.send_message(update.message.chat_id, 'Lucky number: '+str(horoscope.lucky_number))
-    context.bot.send_message(update.message.chat_id, 'Lucky time: '+horoscope.lucky_time)
-    context.bot.send_message(update.message.chat_id, 'Sign compatibility: '+horoscope.compatibility)
-    context.bot.send_message(update.message.chat_id, 'Horoscope valid until: '+str(horoscope.current_date))
-    
+    global started
+    if started == True:
+        if len(context.args) == 1:	
+            sign = ' '.join(context.args).lower()
+            if sign in ['aries','taurus','gemini','leo','cancer','scorpio','pisces','aquarius','virgo','libra','sagittarius','capricorn']:
+                horoscope = pyaztro.Aztro(sign)
+                context.bot.send_message(update.message.chat_id, 'Horoscope for '+str(horoscope.current_date)+'\n\nDescription: '+horoscope.description+'\n\nMood: '+horoscope.mood+'.\nLucky color: '+horoscope.color+'.\nLucky number: '+str(horoscope.lucky_number)+'.\nLucky time: '+horoscope.lucky_time+'.\nSign compatibility: '+horoscope.compatibility+'.')
+            else:
+               context.bot.send_message(update.message.chat_id, 'You need to write an astrological sign (ex: Aries).')
+        elif len(context.args) == 0:
+            context.bot.send_message(update.message.chat_id, 'Usage: /horoscope sign')
+        else:
+           context.bot.send_message(update.message.chat_id, 'Too many arguments. Try again or consult /help') 
+
+# Function used to discern which astrological sign the user has    
 def ask_sign(update, context):
-    correct = True
-    args = ' '.join(context.args).split(' ')
-    month = args[0]
-    day = int(args[1])
-    if month == 12:
-        sign = 'Sagittarius' if (day < 22) else 'Capricorn'
-    elif month == 'january':
-        sign = 'Capricorn' if (day < 20) else 'Aquarius'
-    elif month == 'february':
-        sign = 'Aquarius' if (day < 19) else 'Pisces'
-    elif month == 'march':
-        sign = 'Pisces' if (day < 21) else 'Aries'
-    elif month == 'april':
-        sign = 'Aries' if (day < 20) else 'Taurus'
-    elif month == 'may':
-        sign = 'Taurus' if (day < 21) else 'Gemini'
-    elif month == 'june':
-        sign = 'Gemini' if (day < 21) else 'Cancer'
-    elif month == 'july':
-        sign = 'Cancer' if (day < 23) else 'Leo'
-    elif month == 'august':
-        sign = 'Leo' if (day < 23) else 'Virgo'
-    elif month == 'september':
-        sign = 'Virgo' if (day < 23) else 'Libra'
-    elif month == 'october':
-        sign = 'Libra' if (day < 23) else 'Scorpio'
-    elif month == 'november':
-        sign = 'scorpio' if (day < 22) else 'Sagittarius'
-    else:
-        correct = False
-    print('aaaa')
+    global started
+    day_is_num = False
+    day = 0
 
-    if correct == True:
-    	context.bot.send_message(update.message.chat_id, sign)
+    if started == True:
+        if len(context.args) == 2:
+            args = ' '.join(context.args).split(' ')
+            month_list = ['january','february','march','april','may','june','july','august','september','october','november','december']
+            month = args[0].lower()
+            try:
+                day = int(args[1])
+                day_is_num = True
+            except:
+                context.bot.send_message(update.message.chat_id, 'The birth day has to be a number.')
+            if day_is_num:
+                if month in month_list:
+                    if 0 < day < 32:
+                        if month == month_list[11]:
+                            sign = 'Sagittarius ♐' if (day < 22) else 'Capricorn ♑'
+                        elif month == month_list[0]:
+                            sign = 'Capricorn ♑' if (day < 20) else 'Aquarius ♒'
+                        elif month == month_list[1]:
+                            sign = 'Aquarius ♒' if (day < 19) else 'Pisces ♓'
+                        elif month == month_list[2]:
+                            sign = 'Pisces ♓' if (day < 21) else 'Aries ♈'
+                        elif month == month_list[3]:
+                            sign = 'Aries ♈' if (day < 20) else 'Taurus ♉'
+                        elif month == month_list[4]:
+                            sign = 'Taurus ♉' if (day < 21) else 'Gemini ♊'
+                        elif month == month_list[5]:
+                            sign = 'Gemini ♊' if (day < 21) else 'Cancer ♋'
+                        elif month == month_list[6]:
+                            sign = 'Cancer ♋' if (day < 23) else 'Leo ♌'
+                        elif month == month_list[7]:
+                            sign = 'Leo ♌' if (day < 23) else 'Virgo ♍'
+                        elif month == month_list[8]:
+                            sign = 'Virgo ♍' if (day < 23) else 'Libra ♎'
+                        elif month == month_list[9]:
+                            sign = 'Libra ♎' if (day < 23) else 'Scorpio ♏'
+                        elif month == month_list[10]:
+                            sign = 'Scorpio ♏' if (day < 22) else 'Sagittarius ♐'
+                        context.bot.send_message(update.message.chat_id, 'Your sign is: '+sign)
+                    else:
+                        context.bot.send_message(update.message.chat_id, 'The day number has to be a valid one (between 1 and 31)')
+                else:
+                    context.bot.send_message(update.message.chat_id, "You need to write your birth month's name (ex: December).")
+        elif len(context.args) == 0:
+            context.bot.send_message(update.message.chat_id, 'Usage: /sign birth_month birth_day')
+        else:
+           context.bot.send_message(update.message.chat_id, 'Too many or too few arguments. Try again or consult /help')
 
+# Function to inform the user of the bot usage        
 def help(update, context):
-    context.bot.send_message(update.message.chat_id, 'Initialize the bot with /start')
-    context.bot.send_message(update.message.chat_id, "Select the tarot reading and its theme with /tarot theme")
-    context.bot.send_message(update.message.chat_id, "Get your horoscope with /horoscope sign")
-    context.bot.send_message(update.message.chat_id, "Ask what's your astrological sign with /sign month day")
-        
+    context.bot.send_message(update.message.chat_id, "- Initialize the bot with /start\n- Select the tarot reading and its theme with /tarot theme (Themes available: love, health, work)\n- Get your horoscope with /horoscope sign\n- Ask what's your astrological sign with /sign birth_month birth_day (ex: August 13)")
+
+# Main code        
 def main():
 	TOKEN = "5692200430:AAH5CitxWYf5fAAUj97pzusUnh0Sk-4egl0"
 	updater = Updater(TOKEN, use_context=True)
